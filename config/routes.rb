447f1fbd358601998fcb081a2d10f38b51663b14
clodoaldosouza <clodoaldosouza@hotmail.com>
2016-10-28 21:51:21 -0200

    Iniciando o projeto em rails.

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :drinks
+  resources :pizzas
+  get 'home/index'
+  root 'home#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
